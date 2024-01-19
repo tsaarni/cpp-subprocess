3bb89565c83183a5f404617cd422a34117b3d54e
Tero Saarni <tero.saarni@gmail.com>
2024-01-19 23:25:16 +0200

    Rename the std stream methods to avoid conflict (#9)

--- tests/test-subprocess.cpp
+++ tests/test-subprocess.cpp
@@ -12,7 +12,7 @@ BOOST_AUTO_TEST_CASE(arguments)
     subprocess::popen cmd("echo", {"foo", "bar", "baz"});
 
     std::string buf;
-    std::getline(cmd.stdout(), buf);
+    std::getline(cmd.out(), buf);
 
     BOOST_CHECK_EQUAL(buf, "foo bar baz");
     BOOST_CHECK_EQUAL(cmd.wait(), 0);
@@ -22,12 +22,12 @@ BOOST_AUTO_TEST_CASE(stdio_forwarding)
 {
     subprocess::popen cmd("cat", {});
 
-    cmd.stdin() << "Hello world!" << std::endl;
+    cmd.in() << "Hello world!" << std::endl;
     cmd.close();
-    
+
     std::string buf;
-    std::getline(cmd.stdout(), buf);
-    
+    std::getline(cmd.out(), buf);
+
     BOOST_CHECK_EQUAL(buf, "Hello world!");
     BOOST_CHECK_EQUAL(cmd.wait(), 0);
 }
@@ -36,7 +36,7 @@ BOOST_AUTO_TEST_CASE(return_values)
 {
     subprocess::popen true_cmd("true", {});
     BOOST_CHECK_EQUAL(true_cmd.wait(), 0);
-    
+
     subprocess::popen false_cmd("false", {});
     BOOST_CHECK_EQUAL(false_cmd.wait(), 1);
 }
@@ -50,20 +50,20 @@ BOOST_AUTO_TEST_CASE(bad_command)
 
 BOOST_AUTO_TEST_CASE(pipes)
 {
-    subprocess::popen sort_cmd("sort", {"-r"});    
-    subprocess::popen cat_cmd("cat", {}, sort_cmd.stdin());
-    
-    cat_cmd.stdin() << "a" << std::endl;
-    cat_cmd.stdin() << "b" << std::endl;
-    cat_cmd.stdin() << "c" << std::endl;
+    subprocess::popen sort_cmd("sort", {"-r"});
+    subprocess::popen cat_cmd("cat", {}, sort_cmd.in());
+
+    cat_cmd.in() << "a" << std::endl;
+    cat_cmd.in() << "b" << std::endl;
+    cat_cmd.in() << "c" << std::endl;
     cat_cmd.close();
-    
+
     std::string line;
-    std::getline(sort_cmd.stdout(), line);
+    std::getline(sort_cmd.out(), line);
     BOOST_CHECK_EQUAL(line, "c");
-    std::getline(sort_cmd.stdout(), line);
+    std::getline(sort_cmd.out(), line);
     BOOST_CHECK_EQUAL(line, "b");
-    std::getline(sort_cmd.stdout(), line);
+    std::getline(sort_cmd.out(), line);
     BOOST_CHECK_EQUAL(line, "a");
 
     BOOST_CHECK(cat_cmd.wait() == 0);
